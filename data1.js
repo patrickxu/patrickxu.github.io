263b5ada5b9ba1e203cfe9cfea99c7cf61ee9c9f
patrickxu <patrickxu@college.harvard.edu>
2014-05-01 23:54:31 -0400

    youtube link on

--- data1.js
+++ data1.js
@@ -1246,10 +1246,12 @@ function redraw(i, end) {
   //sets visualizations as false when they reach their end
   if(i==2005 && rockefella == true){
     toptenrevenue = 0;
+    d3.selectAll(".deleteme").remove();
     rockefella = false;
   }
   if(i==2005 && seventiesPlaying == true){
     toptenrevenue = 0;
+    d3.selectAll(".deleteme").remove();
     seventiesPlaying = false;
   }
 
